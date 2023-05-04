e19ec25be4fb19627d65050499c73313cf570fa6
yingying.chang <yingying.chang@shopee.com>
2023-05-04 12:03:34 +0800

    feat: 测试react组件的展示

---
+++ src/reactTransformer/reactComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <VueWrapper :component="Typography.Title" :children="xxxxx" />
+  </div>
+</template>
+
+<script setup>
+import VueWrapper from "./vueWrapper.js";
+import { Typography } from "antd";
+
+export default {
+  name: "CreateButton2",
+  mixins: [],
+};
+</script>
---
+++ src/themes/antd4/Typography.js
@@ -0,0 +1,9 @@
+const data = [
+  {
+    id: "Typegraphy_1",
+    type: "Typegraphy",
+    category: "antd4",
+  },
+];
+
+export default data;
