d13239df2041c32df6b30a081416a141996f2daf
caitlinChang <caitlinChang@163.com>
2023-05-15 08:58:27 +0800

    chore

--- src/reactTransformer/util/constant.js
+++ src/reactTransformer/util/constant.js
@@ -1,7 +1,14 @@
-import { Input, Typography, Radio, Switch } from "antd";
-export const componentList = {
-  "typography-text": Typography.Text,
-  "antd-input": Input,
-  "radio-group": Radio.Group,
-  'antd-switch': Switch,
-};
+import * as antdComponents from "antd";
+// const componentList = Object.keys(antdComponents);
+console.log('antdComponents = ', antdComponents)
+
+Object.keys(antdComponents).forEach((key) => {
+  
+})
+export default [];
+// export const componentList = {
+//   "typography-text": Typography.Text,
+//   "antd-input": Input,
+//   "radio-group": Radio.Group,
+//   'antd-switch': Switch,
+// };
