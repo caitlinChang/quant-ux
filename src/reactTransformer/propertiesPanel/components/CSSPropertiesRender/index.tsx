1bd7f88404d70c4e5e251b222ee156dfeadf8480
yingying.chang <yingying.chang@shopee.com>
2023-08-01 19:31:30 +0800

    feat: CSSProps Render

---
+++ src/reactTransformer/propertiesPanel/components/CSSPropertiesRender/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {} from "antd";
+enum CSSProps {
+  FONT_SIZE = "fontSize",
+  COLOR = "color",
+
+  FILL = "backgroundColor",
+  BORDER = "border",
+  PADDING = "padding",
+  MARGIN = "margin",
+  WIDTH = "width",
+  HEIGHT = "height",
+}
+export default () => {
+  return <div></div>;
+};
